a17a1bed0b60f70a0452ae1fc426b02e29a4e386
Yc3t <ycetaibi@gmail.com>
2025-02-24 20:40:42 +0100

    test

---
+++ test1/src/main.c
@@ -0,0 +1,13 @@
+#include <zephyr/kernel.h>
+#include <zephyr/sys/printk.h>
+#include "myfunction.h"
+
+int main(void)
+{
+  int a = 3, b=4;
+
+  while (1){
+    printk("The sum of %d and %d is %d\n", a,b, sum(a,b));
+    k_msleep(1000);
+  }
+}
